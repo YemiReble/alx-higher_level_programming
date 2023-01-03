d63fb747bd85430d967c6b1f0ea1ecf652570ca9
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-01-03 12:35:30 +0000

    Printing String Index-From to right, right to left

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -2,7 +2,7 @@
 word = "Holberton"
 word_first_3 = format(word[:3])
 word_last_2 = format(word[-2:])
-middle_word = format(word[1:8])
+middle_word = format(word[1:-1])
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word}")
