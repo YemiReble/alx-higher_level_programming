c200a5d3ab5897da57f5a24c099bd03631504878
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-01-04 20:38:08 +0000

    Print ASCII Alphabet

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 for letter in range(ord('a'), ord('z')+1):
-#    letter = chr(letter_code)
-    if letter not in "qe":
-        print(letter, end="")
+        print("{:c}".format(letter), end="")
