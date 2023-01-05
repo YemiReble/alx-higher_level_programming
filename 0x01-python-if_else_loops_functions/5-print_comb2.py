b9592844f7d792b8acb7fc4e3f3175b0612e53e0
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-01-05 12:42:26 +0000

    Print Combination Numbers with double digit

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-number = range(0, 100)
+number = range(0, 99)
 for num in number:
     print('{:02d}'.format(num), end=', ')
-if num == 99:
-    print()
+if num == 98:
+    print('99')
