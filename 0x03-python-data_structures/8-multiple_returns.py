99b524f915689eff19ec6449b32a806ab7a7d38a
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-01-17 14:21:43 +0000

    Check/return the Length of a String and Print the First Char

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) == "":
-        return (None)
-
-    lenght, sen = len(sentence), sentence[0]
-    return lenght, sen
+    if sentence != '':
+        sen = sentence[0]
+ 
+    else:
+        return None
+    return (len(sentence), sen)
