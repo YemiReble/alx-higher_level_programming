1b58eae514bb6c0def052933acb0bc010680bedd
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-01-23 16:26:18 +0000

    Safe Print List of Integers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,7 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[i]), end="")
             alist += 1
 
-        except (IndexError, ValueError, TypeError):
+        except (ValueError, TypeError):
             pass
     print()
     return (alist)
