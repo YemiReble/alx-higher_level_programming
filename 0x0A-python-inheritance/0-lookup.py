e1fa7f553645dae21d199197f82ae34f83dfc934
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-02-07 15:40:41 +0000

    Function that returns the Attributes of a Method

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""lookup the attributes and methods of an object"""
+"""This modle returns the attributes and methods of an object"""
 
 
 def lookup(obj):
+    """Function that lookup the methon of an object"""
+
     list = dir(obj)
     return list
