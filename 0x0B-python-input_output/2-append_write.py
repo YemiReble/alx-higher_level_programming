5bfd61f9311ff1d91a695f9dde57232e4f8a43b0
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-02-10 16:36:33 +0000

    Programe That append new line of text to a file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def append_write(filename="", text=""):
+    """Function that append a new text into a file"""
+    with open(filename, 'a', encoding="UTF-8") as file_name:
+        """Opening file and appeding the with the unicode encoding"""
+        return file_name.write(text)
+        """writing the new line to be appended to the file"""
