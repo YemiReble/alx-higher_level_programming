390426888c57f26203323123655a0bc31e049f47
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-02-10 14:20:19 +0000

    Python Program That Reads A File:

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+""" A python programe that reads any file type"""
+
+
 def read_file(filename=""):
+    """Function that reads file"""
     with open(filename, 'r', encoding="UTF-8") as read_file:
+        """Opening a file and reading it"""
         for line in read_file:
+            """Looping through each line of the file"""
             print(line, end="")
