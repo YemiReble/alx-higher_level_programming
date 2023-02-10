7fd6b949c7eb7440eaf1fb008f623fc7d9fe3e51
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-02-10 16:48:06 +0000

    JSON representation of an object (string)

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" A program that returns the JSON Representation of an object string"""
+import json
+
+
+def to_json_string(my_obj):
+    """Function that returns the JSON Rep - Obj Str"""
+    return json.dumps(my_obj)
