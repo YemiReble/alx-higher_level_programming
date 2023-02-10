46adccb8274556f88a93207135085861cfaae1ca
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-02-10 14:54:41 +0000

    Program that Write into a File

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" A programe that write into a file"""
+
+
+def write_file(filename="", text=""):
+    """Function that write into a file"""
+    with open(filename, 'w', encoding="UTF-8") as file_name:
+        """Openinig a file and writing into it"""
+        file_name.write(text)
+        """Writing from text variable to filename"""
