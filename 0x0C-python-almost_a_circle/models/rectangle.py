ff3d56b62a673c33ac5a7c5f81d91f7a4a01fa92
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-02-13 10:37:54 +0000

    Updates to Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -2,7 +2,8 @@
 """ A class that inherits from Base Class"""
 
 
-Rectangle = __improt__('base').Base
+from models.base import Base
+
 
 class Rectangle(Base):
     """ A class that inherits from Base Class"""
@@ -10,4 +11,83 @@ class Rectangle(Base):
         """ Initaiting Reactangle"""
         self.__width = width
         self.__height = height
+        self.__x = x
+        self.__y = y
+        super().__init__(id)
 
+    """Getter Function List"""
+    @property
+    def width(self):
+        """Get the value of width"""
+        return self.__width
+
+    @property
+    def height(self):
+        """Get the value of height"""
+        return self.__height
+
+    @property
+    def x(self):
+        """Get the value of x"""
+        return self.__x
+
+    @property
+    def y(self):
+        """ Get the value of y"""
+        return self.__y
+
+    """Setter Function List"""
+    """For width"""
+    @width.setter
+    def width(self, value):
+        """Fucntion that set conditions for width and raise exceptions
+        if values are not as expected"""
+        if (type(value) is not int):
+            # if not int(value):
+            raise TypeError("{} must be an integer".format('width'))
+
+        if value <= 0:
+            raise ValueError("{} must be > 0".format('width'))
+
+        self.__width = value
+
+    """For height"""
+    @height.setter
+    def height(self, value):
+        """Function that set conditions for height and raise exceptions
+        if value are not as expected"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format('height'))
+
+        if value <= 0:
+            raise ValueError("{} must be > 0".format('height'))
+
+        self.__height = value
+
+    @x.setter
+    def x(self, value):
+        """Function that set conditions for x value and raise exceptions if
+        value are not as expected."""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format('x'))
+
+        if value <= 0:
+            raise ValueError("{} must be > 0".format('x'))
+
+        self.__x = value
+
+    @y.setter
+    def y(self, value):
+        """Function that set condition for y value and raise exceptions if
+        value are not as expected."""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format('y'))
+
+        if value <= 0:
+            raise ValueError("{} must be > 0".format('y'))
+
+        self.__y = value
+
+    def area(self):
+        """ A Public method that retuns the area of a rectangle"""
+        return (self.__width * self.__height)
