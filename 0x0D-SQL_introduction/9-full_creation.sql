9a89c7e63ab02fe1bc400f4a5513352c933f7a47
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-03-15 21:31:17 +0000

    A Full DateBase With Datas

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -5,12 +5,13 @@
 --      name VARCHAR(256)
 --      score INT
 
-CREATE TABLE IF EXISTS second_table (id INT, name 
+CREATE TABLE IF NOT EXISTS second_table (id INT, name 
     VARCHAR(256), 
     score INT
 );
 
-INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
-INSERT INTO second_table (id, name, score) VALUES (2, 'Alex',3);
-INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
-INSERT INTO second_table (id, name, score) VALUES (4, 'Goerge', 8);
+INSERT INTO second_table (id, name, score) 
+VALUES (1, 'John', 10),
+    (2, 'Alex', 3),
+    (3, 'Bob', 14),
+    (4, 'Goerge', 8);
