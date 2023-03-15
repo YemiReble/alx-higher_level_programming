b02934c10b0b307db6587fda3d180fd55366d444
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-03-15 17:18:24 +0000

    Insert Values To A Table Row By Row

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,6 @@
+-- This Script Insert some info into my database
+-- Use the Table created in the table inside hbtn_0c_0 DataBase
+-- Running this file continously add more Rows to the Table
+-- Assigning Values to ID and Name.
+
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
