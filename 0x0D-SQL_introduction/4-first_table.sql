9c811a0d1ba6cdc5ecb93b5710e9c7fed2f0a694
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-03-15 15:58:50 +0000

    Add a New Table

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,9 @@
+-- This Script Create my first table on my database
+-- Create a new table inside hbtn_0c_0 DataBase if it
+-- does not exist and add some descriptions
+-- ID and Name.
+
+CREATE TABLE IF NOT EXISTS first_table(
+    id INT NOT NULL,
+    name VARCHAR(256)
+);    
