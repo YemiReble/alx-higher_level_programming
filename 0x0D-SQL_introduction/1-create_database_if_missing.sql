ab77aa88ebaebe5c58129ff28816101e9e5f0e92
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-03-15 14:50:08 +0000

    Create a DataBase if not exist

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,4 @@
--- This Script create A DataBase hbtn_0c_0 In my Server
-CREATE DATABASE hbtn_0c_0
+-- This Script create A DataBase hbtn_0c_0 on my Server
+-- If the database hbtn_0c_0 doesn’t exist, script does not fail
+
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0
