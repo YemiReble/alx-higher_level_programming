8cebedf05bcdb64d3e31c83fc1ded141a6df6d3c
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-03-16 23:32:13 +0000

    Created A new Table That Referenced an Id from Another Table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -12,9 +12,9 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
-        id INT,
-        state_id INT NOT NULL AUTO_INCREMENT,
+        id INT NOT NULL AUTO_INCREMENT,
+        state_id INT NOT NULL,
         name VARCHAR(256) NOT NULL,
-        PRIMARY KEY (state_id),
-        FOREIGN KEY (id) REFERENCES states(id)
+        PRIMARY KEY (id),
+        FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id)
 );
