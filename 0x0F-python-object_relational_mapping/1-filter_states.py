a40b72623f960d0f1d78718c0fe41f14372907d8
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-04-14 14:37:23 +0000

    Display All Data That Start With 'N' In The Table

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,7 +11,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY 'N%'\
+        ORDER BY states.id ASC")
     db_row = cur.fetchall()
     for row in db_row:
         print(row)
