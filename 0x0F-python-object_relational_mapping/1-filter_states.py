c0e03f617887948567fa3ac1413d01ad69c371bb
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-04-14 14:32:21 +0000

    Display All Data That Start With 'N' In The Table

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This is a script that displays all data in
+database 'hbtn_0e_0_usa that start with 'N'
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    db_row = cur.fetchall()
+    for row in db_row:
+        print(row)
