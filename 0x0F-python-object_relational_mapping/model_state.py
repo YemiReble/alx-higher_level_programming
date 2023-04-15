55c1439a8707e2335e9e0ef2134e2307683cefd5
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-04-15 15:53:45 +0000

    A File That Create A New Table

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+ A python file that contains the class definition of
+ a State and an instance Base = declarative_base():
+ State class:
+ links to the MySQL table states
+ class attribute id that represents a column of
+ an auto-generated, unique integer, can’t be
+ null and is a primary key
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class State(Base):
+
+    __table_name__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+    # Establishing connection to the local-host
+    engine = create_engine('mysql://localhost:3306')
+
+    # Import all class that inherits from base
+    Base.metadata.create_all(engine)
