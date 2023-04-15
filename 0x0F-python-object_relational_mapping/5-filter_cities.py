549b60ddcb7187eb2a52de2f097eded38ada16c6
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-04-15 14:04:01 +0000

    Get an Argument that search for the name of the state provided

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -15,10 +15,13 @@ if __name__ == "__main__":
 
     db = MySQLdb.connect(
         host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name FROM cities JOIN states ON\
-        cities.state_id=states.id WHERE states.name LIKE BINARY\
-        %(states_name)s ORDER BY cities.id ASC", {'state_name': argv[4]})
-    new_var = cur.fetchall()
-    if new_var is not None:
-        print(", ".join(row[1] for row in new_var))
+    # cur = db.cursor()
+    with db.cursor() as cur: 
+    # Found this out after 2 hours of debugging
+    # But still not the problem :(
+        cur.execute("SELECT cities.id, cities.name FROM cities JOIN states ON\
+            cities.state_id=states.id WHERE states.name LIKE BINARY\
+            %(states_name)s ORDER BY cities.id ASC", {'states_name': argv[4]})
+        new_var = cur.fetchall()
+        if new_var is not None:
+            print(", ".join([row[1] for row in new_var]))
