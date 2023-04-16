680adb10a4702b699d11565bbdc8fe2833ed7362
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-04-16 16:54:59 +0000

    A File That Create A New Table

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -11,7 +11,6 @@
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
 
 Base = declarative_base()
 
@@ -19,11 +18,5 @@ Base = declarative_base()
 class State(Base):
 
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-
-    # Establishing connection to the local-host
-    # engine = create_engine('mysql://localhost:3306')
-
-    # Import all class that inherits from base
-    # Base.metadata.create_all(engine)
