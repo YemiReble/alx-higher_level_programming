15654ab2ea9cac431caa10c268ea43d75db289af
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-04-29 13:41:46 +0000

    Body Size of a HTTP Request

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 # This is a bash script that takes in a URL, sends a request to that URL, and displays the
-# size of the body of the response
-
 curl -s -o /dev/null -w '%{size_download}\n' https://"$1"
