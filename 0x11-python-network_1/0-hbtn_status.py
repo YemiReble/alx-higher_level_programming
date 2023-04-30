c5f8816752576efaecc81f2aad6ddd6ae91c4370
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-04-30 20:26:32 +0000

    Puython Url Request

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This is a python script that send a request
+to a URL and return the out put to stdout
+using <pipe> filename.py | cat -e
+"""
+
+import urllib.request
+
+url = "https://alx-intranet.hbtn.io/status"
+with urllib.request.urlopen(url) as response:
+    res = response.read()
+    print("Body Response:")
+    print("    - type: {}".format(type(res)))
+    print("    - content: {}".format(res))
+    print("    - utf8 content: {}".format(res.decode('utf-8')))
