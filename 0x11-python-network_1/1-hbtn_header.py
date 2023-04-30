ab60c0e4404d1d9d29b448d46b7569d336676614
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-04-30 21:29:47 +0000

    Display the value of the X-Request-Id variable

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """
 This python script sends a request to
-the URL and displays the value of
-the X-Request-Id variable found in the header of
-the response
+the URL and displays the value of the X-Request-Id 
+variable found in the header of the response
 """
 
 
