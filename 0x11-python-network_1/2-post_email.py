58b142ee86fc5d8cc6f6715370d284e68681e8f8
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-04-30 21:49:44 +0000

    A Python script that sends a POST request to the passed URL with the email as a parameter

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This is a script that takes in url and email
+as input argument and sent a POST request to it
+to displays the body of the response
+"""
+
+import urllib.request
+import urllib.parse
+import sys
+
+
+if __name__ == "__main__":
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = urllib.parse.urlencode({"email": email}).encode()
+    req = urllib.request.Request(url, data=data, method="POST")
+
+    with urllib.request.urlopen(req) as response:
+        body = response.read().decode("utf-8")
+        print(body)
