12fe14af73bd9d0007d884c9cd38376b7eb0e6d3
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-05-01 20:55:48 +0000

    Post Email

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This is a python script that sends a POST
+request to the passed URL with the email as a parameter,
+and finally displays the body of the response.
+"""
+
+import sys
+import request
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    inst = {'email': email}
+    resp = requests.get(url, data=inst)
+
+    print(resp.content)
