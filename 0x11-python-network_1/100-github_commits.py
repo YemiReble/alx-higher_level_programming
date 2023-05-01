3862097e22144596f9e8be474416f5a4755f8e6d
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-05-01 23:08:46 +0000

    A program that prints last 10 commit message

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This is a python script that takes in 2 argument
+and let you print out the last 10 github commit
+message
+"""
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+
+    response = requests.get(url)
+    commits = response.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
