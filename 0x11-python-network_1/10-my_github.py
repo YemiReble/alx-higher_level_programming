8059e71d82cbee102f649c205fbd94cb3230adbd
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-05-01 22:50:14 +0000

    GitHub Id Get Request

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This is a python script that takes my username and password
+using Github Api to display my Id.
+"""
+
+import requests
+import sys
+from requests.auth import HTTPBasicAuth
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    var = HTTPBasicAuth((username, password))
+
+    url = "https://api.github.com/user"
+    response = requests.get(url, auth=var)
+
+    json_response = response.json()
+    print(json_response["id"])
