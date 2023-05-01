8d78f2126a9ac198f4d1875537ba11f520ad9e1d
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-05-01 23:40:50 +0000

    Search Api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -10,19 +10,20 @@ import sys
 
 if __name__ == "__main__":
 
-	if len(sys.argv) > 1:
-		q = sys.argv[1]
-	else:
-		q = ""
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+    else:
+        q = ""
 
-	payload = {'q': q}
-	response = requests.post('http://0.0.0.0:5000/search_user', data=payload)
+    payload = {'q': q}
+    response = requests.post('http://0.0.0.0:5000/search_user', data=payload)
 
-	try:
-		json_response = response.json()
-		if json_response: print("[{}] {}".format(json_response.get(
+    try:
+        json_response = response.json()
+        if json_response:
+            print("[{}] {}".format(json_response.get(
                 "id"), json_response.get("name")))
-		else:
-			print("No result")
-	except ValueError:
-		print("Not a valid JSON")
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
