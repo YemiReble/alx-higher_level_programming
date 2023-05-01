1347a11e2fcbf0b61a1b897e0c44cffbb9c796e5
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-05-01 21:18:38 +0000

    Status Error Code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This is a python script that sends a request to the URL
+and displays the body of the response If the HTTP status code 
+is greater than or equal to 400, print: Error code: followed 
+by the value of the HTTP status code
+"""
+
+import sys
+import requests
+
+
+if __name__ == "__main___":
+    url = sys.argv[1]
+
+    data = requests.get(url)
+    
+    if data.status_code >= 400:
+        print("Error code: {}".format(data.status_code))
+    else:
+        print(data.text)
