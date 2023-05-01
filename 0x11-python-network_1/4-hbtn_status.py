582ac0472afa82e7621f62baed6e78eedf38db72
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-05-01 16:18:14 +0000

    HBTN Status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,15 +2,16 @@
 """
 This is a python script that send an HTTP
 Request to a Url and print out the receipt
+using the requests Model
 """
 
-import urllib.request
+import requests
 
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
 
-    with urllib.request.urlopen(url) as response:
-        data = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(url)))
-        print("\t- content: {}".format(data.decode("utf-8")))
+    data = requests.get(url)
+    # data = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(data.content)))
+    print("\t- content: {}".format(data.content))
